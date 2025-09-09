1296645141d084c62b91b10270bcae712196da4f
Nikos Iliakis <nikoulai94@hotmail.com>
2025-09-09 14:32:02 +0800

    Introduce error instead of error  messages

--- src/contracts/DAO.sol
+++ src/contracts/DAO.sol
@@ -1,17 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.30;
 
 import "../interfaces/IReferenda.sol";
 
 contract DAO is IReferenda {
+    error InvalidReferendaId(uint256 referendaId);
+    error VotingNotFinished(uint256 blockNumber, uint256 validUntil);
+    error VotingPeriodExpired(uint256 blockNumber, uint256 validUntil);
+    error AlreadyVoted(address voter);
+    error AlreadyProposed(address proposer);
+
     struct Proposal {
         string title;
         string description;
     }
 
     modifier _noActiveProposal() {
-        require(!hasActiveProposal[msg.sender], "User has active proposal");
+        require(!hasActiveProposal[msg.sender], AlreadyProposed(msg.sender));
         _;
     }
 
@@ -35,10 +41,11 @@ contract DAO is IReferenda {
         defaultValidUntil = _defaultValidUntil;
     }
 
-    function propose(
-        string calldata _title,
-        string calldata _description
-    ) external _noActiveProposal /*_isVoter */ returns (uint256) {
+    function propose(string calldata _title, string calldata _description)
+        external
+        _noActiveProposal /*_isVoter */
+        returns (uint256)
+    {
         hasActiveProposal[msg.sender] = true;
         uint256 id = counter;
         counter++;
@@ -48,15 +55,16 @@ contract DAO is IReferenda {
         return id;
     }
 
-    function vote(uint256 referendaId, bool isFor /* _isVoter */) external {
-        require(counter > referendaId, "Referenda id is invalid");
+    function vote(uint256 referendaId, bool isFor /* _isVoter */ ) external {
+        require(counter > referendaId, InvalidReferendaId(referendaId));
+
         Referenda storage referenda = referendas[referendaId];
-        require(
-            referenda.validUntil > block.number,
-            "Window for voting expired"
-        );
-        require(!referenda.votes[msg.sender], "Already voted");
+
+        require(referenda.validUntil > block.number, VotingNotFinished(block.number, referenda.validUntil));
+        require(!referenda.votes[msg.sender], AlreadyVoted(msg.sender));
+
         referenda.votes[msg.sender] = true;
+
         if (isFor) {
             referenda.backing.yay++;
         } else {
@@ -65,14 +73,18 @@ contract DAO is IReferenda {
     }
 
     function resolve(uint256 referendaId) external {
-        require(counter > referendaId, "Referenda id is invalid");
+        require(counter > referendaId, InvalidReferendaId(referendaId));
+
         Referenda storage referenda = referendas[referendaId];
+
         require(block.number >= referenda.validUntil, "Voting is not finished");
+        require(block.number >= referenda.validUntil, VotingNotFinished(block.number, referenda.validUntil));
 
         if (referenda.backing.yay > referenda.backing.nay) {
             referenda.resolution = Resolution.ACCEPTED;
         } else {
             referenda.resolution = Resolution.REJECTED;
         }
+        //todo clear proposer's state
     }
 }
